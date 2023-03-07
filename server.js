1a4ef2e71f08ca1750d9826fb71533a1e79e24b5
JamesRJGray <jamesrjgray@gmail.com>
2023-03-06 20:12:41 -0600

    added dependencies, server.js initial setup with port and induces

---
+++ server.js
@@ -0,0 +1,38 @@
+// DEPENDENCIES
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+require('dotenv').config()
+
+// GLOBAL CONFIRGURATION
+
+// CONNECT TO MONGO
+
+// ERROR/SUCCESS LOGS
+
+// MIDDLEWARE
+
+// SEED DATA FUNCTION
+
+// ROUTES - I.N.D.U.C.E.S
+
+// I
+
+// N
+
+// D
+
+// U
+
+// C
+
+// E
+
+// S
+
+// PORT
+const PORT = 3000
+app.listen(PORT, () => {
+    console.log('Hello Seattle, I\'m listening... on PORT', PORT)
+})
