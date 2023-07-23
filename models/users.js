b620af9112016c328a2932d3e13c37031b60abba
James Gray <jamesrjgray@gmail.com>
2023-07-22 22:16:32 -0500

    updated user model with comics schema in the new comics field

--- models/users.js
+++ models/users.js
@@ -1,8 +1,18 @@
 const mongoose = require('mongoose')
 
+const comicsSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    number: Number,
+    volume: Number,
+    publisher: String,
+    year: Number,
+})
+
 const userSchema = new mongoose.Schema({
     username: {type: String, required: true, unique: true},
-    password: String
+    password: {type: String, required: true, unique: true},
+    comics: [comicsSchema]
 })
 
 const User = mongoose.model('User', userSchema)
